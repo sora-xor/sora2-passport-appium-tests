52a52e7d71e1992352f457f0baac9dc5ddd88b10
Ekaterina Semenova <109272254+adaagava@users.noreply.github.com>
2022-09-27 12:09:25 +0200

    update for appium 2
    update for appium 2

--- src/main/FirstTest.java
+++ src/main/FirstTest.java
@@ -24,22 +24,25 @@ public class FirstTest extends BaseTest {
 	private String bundlId = "co.jp.soramitsu.sora.dev";
 
     @BeforeSuite
-    public void setUp() throws Exception {    	
+    public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "iOs");
+				capabilities.setCapability("automationName", "XCUITest");
         capabilities.setCapability("appium:platformVersion", "16.0");
-        capabilities.setCapability("appium:deviceName", "iPhone 14 Pro Max");
+        capabilities.setCapability("appium:deviceName", "iPhone 14 Pro");
         capabilities.setCapability("appium:includeSafariInWebviews", true);
         capabilities.setCapability("appium:newCommandTimeout", 3600);
         capabilities.setCapability("appium:connectHardwareKeyboard", true);
+				capabilities.setCapability("appium:wdaLaunchTimeout", 40000);
+
         URL remoteUrl = new URL("http://0.0.0.0:4723/wd/hub");
 
         driver = new IOSDriver(remoteUrl, capabilities);
-	 	try 
+	 	try
  		{
 	 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 	 		throw new InterruptedException();
- 		} 
+ 		}
 	 	catch(InterruptedException e)
  		{
 	 		System.out.println(e);
@@ -59,26 +62,26 @@ public class FirstTest extends BaseTest {
         // Our bondle ID is co.jp.soramitsu.sora.dev.
 	 	Assert.assertTrue(driver.isAppInstalled(bundlId));
     };
-    
+
  @Test
  	public void testCreateAccount () {
 	 	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 	 	// Check we have big logo.
 	 	Assert.assertNotNull(driver.findElement(By.name("SORA Logo Big")), "Something went wrong, we don't get big logo at the app start");
-	 	
+
 	 	//Creating the account.
 	 	driver.findElement(By.name("CREATE ACCOUNT")).click();
 	 	driver.findElement(By.name("Account Name")).sendKeys("Test User" + timestamp.getTime());
 	 	driver.findElement(By.name("CONTINUE")).click();
-	 	
+
 	 	//Check we have pop-up window
 	 	Assert.assertNotNull(driver.findElement(By.xpath("//XCUIElementTypeAlert[@name=\"Do not take screenshots\"]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeScrollView[1]/XCUIElementTypeOther[1]")), "We didn't get the screenshots alert");
 	 	driver.findElement(By.name("OK")).click();
-	 	
+
 	 	//Check we are on mnomonic screen
 	 	WebElement alertSign = driver.findElement(By.name("iconWarningBig"));
 	 	Assert.assertNotNull(alertSign, "We didn't get the red alert sign");
-	 	
+
 	 	//Remember the mnemonic
 	 	ArrayList<String> keyWords = new ArrayList<String>();
 	 	for (int i =  1; i <= 2; i++)
@@ -90,7 +93,7 @@ public class FirstTest extends BaseTest {
 	 	}
 		driver.findElement(By.name("CONTINUE")).click();
 		System.out.println(keyWords.toString()); //Just print memonic for debug here
-		
+
 		//Check we are in mnemonic reorder screen
 		Assert.assertNotNull(driver.findElement(By.xpath("//XCUIElementTypeApplication[@name=\"SORA Dev\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeScrollView/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]")), "We ddn't get to the mnemoic reorder screen");
 		Assert.assertNotNull(driver.findElement(By.name(keyWords.get(1))));
@@ -102,28 +105,28 @@ public class FirstTest extends BaseTest {
 	 	}
 		Assert.assertTrue(driver.findElement(By.name("CONFIRM")).isDisplayed());
 	 	driver.findElement(By.name("CONFIRM")).click();
-	 	
+
 	 	//Check that we are at the pin code setup screen
 	 	Assert.assertNotNull(driver.findElement(By.name("Setup Pin Code")), "We don't get to the pin code screen");
-	 	
+
 	 	//Insert 123456 pin
 	 	for (int i =  1; i <= 6; i++)
 	 	{
 	 		driver.findElement(By.name(""+i)).click();
 	 	}
-	 	
+
 	 	//Check that we are on confirm pin code screen
 	 	Assert.assertNotNull(driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name=\"Confirm Pin Code\"]")), "We don't get to the confirm pin code screen");
-	 	
+
 	 	//Insert 123456
 	 	for (int i =  1; i <= 6; i++)
 	 	{
 	 		driver.findElement(By.name(""+i)).click();
 	 	}
-	 	
+
 	 	//Check we are on the main screen. The main screen can be used in different tests so it's in the screenn package.
 	 	MainScreen mainScreen = new MainScreen(driver);
 	 	mainScreen.validateHeader();
  	};
- 
+
  }
