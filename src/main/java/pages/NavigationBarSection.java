8d9dbcccb4ce9f137ce349be98bd3f10a8e818ad
Vladislav Gorbunov <gorbunov@soramitsu.co.jp>
2022-10-28 17:32:38 +0300

    add NavigationBarSection

--- src/main/java/pages/NavigationBarSection.java
+++ src/main/java/pages/NavigationBarSection.java
@@ -7,4 +7,20 @@ public class NavigationBarSection {
 
     @AndroidFindBy(id = "jp.co.soramitsu.sora:id/wallet_nav_graph")
     private SelenideElement walletNavGraph;
+
+    @AndroidFindBy(id = "jp.co.soramitsu.sora.develop:id/invest_nav_graph")
+    private SelenideElement investNavGraph;
+
+    @AndroidFindBy(id = "jp.co.soramitsu.sora:id/polkaswap_nav_graph")
+    private SelenideElement polkaswapNavGraph;
+
+    @AndroidFindBy(id = "jp.co.soramitsu.sora.develop:id/fabMain")
+    private SelenideElement fabMainSwap;
+
+    @AndroidFindBy(id = "jp.co.soramitsu.sora.develop:id/activities_nav_graph")
+    private SelenideElement activityNavGraph;
+
+    @AndroidFindBy(id = "jp.co.soramitsu.sora:id/profile_nav_graph")
+    private SelenideElement profileNavGraph;
+
 }
