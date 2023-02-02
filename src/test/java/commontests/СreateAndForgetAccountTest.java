fd42a02e5089771e92750bf7d655577e2a78296d
Vladislav Gorbunov <gorbunov@soramitsu.co.jp>
2023-02-02 20:00:16 +0300

    Added СreateAndForgetAccountTest, refactoring

---
+++ src/test/java/commontests/СreateAndForgetAccountTest.java
@@ -0,0 +1,31 @@
+package commontests;
+
+import configs.TestConfig;
+import infrastructure.CoreTestCase;
+import lombok.extern.log4j.Log4j2;
+import org.testng.annotations.Test;
+import pages.*;
+import pages.account.*;
+
+import static com.codeborne.selenide.appium.ScreenObject.screen;
+
+
+@Log4j2
+public class ForgetAccountTest extends CoreTestCase {
+
+    @Test
+    public void ForgetAccountTest() {
+
+        WelcomePage welcomePage = screen(WelcomePage.class);
+        NameYourAccountPage nameYourAccountPage = welcomePage.goToCreateAccountPage();
+        PayAttentionPage payAttention = nameYourAccountPage.enterAccountName("Account Test");
+        PassphrasePage passphrasePage = payAttention.confirmPayAttention();
+        PinCodePage pinCodePage = passphrasePage.skipPassPhrase();
+        WalletPage walletPage = pinCodePage.enterAndConfirmPinCode();
+        walletPage.walletPageIsOpen();
+        MorePage morePage =  walletPage.getNavigationBarSection().goToMorePage();
+        AccountsPage accountsPage = morePage.goToAccounts();
+        PinCodePage pinCodePage1 = accountsPage.forgetAccount();
+        pinCodePage1.enterPinCode();
+    }
+}
