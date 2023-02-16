f033084a1f49f948cb0176af9512404a6c8a22a7
Vladislav Gorbunov <gorbunov@soramitsu.co.jp>
2023-02-16 16:58:57 +0300

    pom.xml changes

--- src/main/java/pages/MorePage.java
+++ src/main/java/pages/MorePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.iOSXCUITFindBy;
