de85b1a5678ff8c8d81c41dcd5439cfdc1d2f5f0
aelikka <aelikka@gmail.com>
2023-11-21 15:50:23 +0500

    change account name test

--- src/main/java/pages/account/AccountsPage.java
+++ src/main/java/pages/account/AccountsPage.java
@@ -7,6 +7,8 @@ import com.codeborne.selenide.SelenideElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 import lombok.extern.log4j.Log4j2;
+import static org.assertj.core.api.Assertions.*;
+
 
 
 @Log4j2
@@ -29,15 +31,25 @@ public class AccountsPage {
     @iOSXCUITFindBy(accessibility = "Forget account")
     private SelenideElement forgetAccountBtn;
 
+    @AndroidFindBy(className = "android.widget.EditText")
+    private SelenideElement accountName;
+
     public PinCodePage forgetAccount() {
         log.info("Open Account menu");
         openAccountMenuItem.click();
-       if (isAndroid()) {
-    	   openAccountOptios.shouldBe(Condition.visible).click();
-       }
+        if (isAndroid()) {
+            openAccountOptios.shouldBe(Condition.visible).click();
+        }
         log.info("Tap Forget account");
         forgetAccountBtn.click();
         return screen(PinCodePage.class);
     }
+
+    public void changeAccountName() {
+        log.info("Open Account menu");
+        openAccountMenuItem.click();
+        accountName.sendKeys("ChangedName");
+        assertThat(accountName.getText()).isEqualTo("ChangedName");
     }
+}
 
