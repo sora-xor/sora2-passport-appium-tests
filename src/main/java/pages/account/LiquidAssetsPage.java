c04f64a2281f69000ce409ff341e90d280668ffb
edarintseva <aelikka@gmail.com>
2023-12-04 11:46:05 +0500

    deleted step that failed. it is unnecessary for the case, but should be investigated in the future

--- src/main/java/pages/account/LiquidAssetsPage.java
+++ src/main/java/pages/account/LiquidAssetsPage.java
@@ -33,7 +33,7 @@ import static infrastructure.Platform.isIOS;
 @Log4j2
 public class LiquidAssetsPage {
 
-    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.widget.ScrollView/android.view.View[2]/android.widget.Button[1]")
+    @AndroidFindBy(id = "jp.co.soramitsu.sora.develop:id/SendButton")
     @iOSXCUITFindBy(accessibility = "Send")
     private SelenideElement sendTokenBtn;
     @AndroidFindBy(id = "jp.co.soramitsu.sora.develop:id/TokenN0")
