ce22029695242c89c5d37a6890eefabd69fd4706
edarintseva <aelikka@gmail.com>
2023-12-19 21:06:53 +0500

    referral tests partly fixed

--- src/test/java/commontests/SetReferrerLink.java
+++ src/test/java/commontests/SetReferrerLink.java
@@ -2,6 +2,7 @@ package commontests;
 
 import configs.TestConfig;
 import infrastructure.CoreTestCase;
+import io.qameta.allure.Issue;
 import org.testng.annotations.Test;
 import pages.*;
 import pages.account.AccountsPage;
@@ -10,12 +11,14 @@ import pages.account.PinCodePage;
 public class SetReferrerLink extends CoreTestCase {
 
     @Test
+    @Issue("link for ios to be added")
     public void setReferrerLink() {
         WalletPage walletPage = WalletPage.createNewAccount();
         MorePage morePage = walletPage.getNavigationBarSection().goToMorePage();
         ReferralProgramPage referralProgramPage = morePage.goToReferralProgramPage();
         ActivityPage activityPage = referralProgramPage.setRefferrersLink(TestConfig.config.referrerLink1());
         activityPage.checkSetReffererTransaction();
+        //ios crush on this step
         referralProgramPage.returnToMorePage();
         AccountsPage accountsPage = morePage.goToAccounts();
         PinCodePage enterCodePage = accountsPage.forgetAccount();
